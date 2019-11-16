04d58b88312d4b30264c70cccb15943533ff59a8
fionajryan <fiona.ryan@mail.mcgill.ca>
2019-11-15 23:08:58 -0500

    push

---
+++ src/Code.java
@@ -0,0 +1,36 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Code extends JPanel{
+
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Drawing Lines");
+        frame.setBackground(Color.white);
+        frame.setSize(200, 200);
+        Code panel = new Code();
+        frame.add(panel);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public Code(){
+        repaint();
+    }
+
+    public void paintComponent(Graphics g){
+        g.setColor(Color.RED);
+        g.drawLine(0,0,100,100);
+    }
+    public void loop(){
+        /*
+            Call script to get image
+            process image
+            get values
+            calculate change
+            draw
+            repetez
+
+         */
+    }
+}
