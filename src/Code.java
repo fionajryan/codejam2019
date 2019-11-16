019a736a96d09a7a9ef1d7a3741e54868318e792
tmjedi <tmjedi51@gmail.com>
2019-11-16 11:28:18 -0500

    idk

--- src/Code.java
+++ src/Code.java
@@ -1,10 +1,13 @@
 import javax.swing.*;
 import java.awt.*;
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Scanner;
 
 public class Code extends JPanel{
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         JFrame frame = new JFrame("Drawing Lines");
         frame.setBackground(Color.white);
         frame.setSize(200, 200);
@@ -33,5 +36,15 @@ public class Code extends JPanel{
             repetez
 
          */
+        try {
+            Scanner scan = new Scanner(new BufferedReader(new FileReader("C:\\Users\\tmjed\\PycharmProjects\\CodeJam\\venv\\Scripts\\rsc\\vals.txt")));
+            scan.useDelimiter(",");
+            LinkedList<Double> list = new LinkedList<>();
+            while(scan.hasNextFloat()){
+                list.add(scan.nextDouble());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
